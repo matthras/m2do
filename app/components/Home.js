ae4bc45d75006b81f0d0f548447a2c12bb67f079
matthras <matthew.y.p.mack@gmail.com>
2018-01-17 23:22:39 +1100

    added initial stuff to state for testing, created child component that somehow still doesn't work

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,12 +3,44 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Home.css';
 
+class TaskList extends Component {
+  render() {
+    const taskList = this.props.list.map( (item) => {
+      <span>{item.description}</span>
+    })
+    return(
+      <div id="taskList">
+        {taskList}
+      </div>
+    )
+  }
+}
+
+
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [
+        {
+          description: 'Work on this stupid app',
+          date: '26-12-1989',
+          priority: 'High'
+        },
+        {
+          description: 'Work on other stupid stuff',
+          date: '17-01-2018',
+          priority: 'Low'
+        }
+      ]
+    };
+  }
   render() {
     return (
       <div>
         <div className={styles.container} data-tid="container">
           <h2>Home is where the heart is</h2>
+          <TaskList list={this.state.list} />
         </div>
       </div>
     );
